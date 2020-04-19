c509bc0d8a48a6331e5c16837f4c8f8977991de1
“Shubham” <shubhampattar2@gmail.com>
2020-04-19 18:36:50 +0530

    updated upto daily wage

--- gitTask/empWageComputation.sh
+++ gitTask/empWageComputation.sh
@@ -6,5 +6,12 @@ then
 	echo "Employee is Absent";
 else
 	echo "Employee is Present";
+	wagePerHr=20;
+	workHr=8;
+	partTimeHr=8;
+	worked=$((RANDOM%9))
+	dailyWage=$((worked*wagePerHr))
+	echo "Today's Working Hour"$worked;
+	echo "Employee's Daily Wage : "$dailyWage;
 fi
 
