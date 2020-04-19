84157277c1afb89ffef65afc81091d0999d04c35
“Shubham” <shubhampattar2@gmail.com>
2020-04-19 18:54:20 +0530

    Updated with new functionallity

--- gitTask/empWageComputation.sh
+++ gitTask/empWageComputation.sh
@@ -9,9 +9,24 @@ else
 	wagePerHr=20;
 	workHr=8;
 	partTimeHr=8;
-	worked=$((RANDOM%9))
-	dailyWage=$((worked*wagePerHr))
-	echo "Today's Working Hour"$worked;
-	echo "Employee's Daily Wage : "$dailyWage;
+	workingDays=20;
+	monthlyWage=0;
+	totalWorkedHR=0;
+	for(( i=0; i<=$workingDays; i++ ))
+	do
+		worked=$((RANDOM%9));
+		dailyWage=$((worked*wagePerHr));
+		monthlyWage=$((monthlyWage+dailyWage));
+		totalWorkedHR=$((totalWorkedHR+worked));
+
+	done
+	echo "Employee's total working Hours in this month "$totalWorkedHR;
+	echo "Employee's Monthly Wage : "$monthlyWage;
+
+
+
+	#echo "Today's Working Hour"$worked;
+	#echo "Employee's Daily Wage : "$dailyWage;
+	
 fi
 
